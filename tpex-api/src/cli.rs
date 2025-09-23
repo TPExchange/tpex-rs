d17f5e615f0dc537f867dc17205a1e44b7a6b488
Cyclic3 <cyclic3.git@gmail.com>
2025-09-23 12:37:26 +0100

    Unify cli

---
+++ tpex-api/src/cli.rs
@@ -0,0 +1,67 @@
+use std::pin::pin;
+
+use clap::Parser;
+use futures::StreamExt;
+use tpex_api::Token;
+
+#[derive(clap::Subcommand)]
+enum Command {
+    /// Stream the state to stdout
+    Mirror,
+    /// Create an atomically updated cache file of the FastSync data
+    CacheFastsync {
+        path: String
+    }
+}
+
+#[derive(clap::Parser)]
+struct Args {
+    /// The remote TPEx api endpoint
+    #[arg(long, env = "TPEX_URL")]
+    endpoint: reqwest::Url,
+    /// The token for that remote
+    #[arg(long, env = "TPEX_TOKEN")]
+    token: Token,
+    #[command(subcommand)]
+    command: Command
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    match args.command {
+        Command::Mirror => {
+            let mut next_id = 1;
+            'next: loop {
+                let remote = tpex_api::Remote::new(args.endpoint.clone(), args.token);
+                let Ok(state_stream) = remote.stream_state(next_id).await else {continue;};
+                let mut state_stream = pin!(state_stream);
+                while let Some(next) = state_stream.next().await {
+                    let Ok(next) = next else { continue 'next; };
+                    if next.id != next_id {
+                        continue 'next;
+                    }
+                    serde_json::to_writer(&std::io::stdout(), &next).expect("Failed to reserialise wrapped action");
+                    println!();
+                    next_id += 1;
+                }
+            }
+        },
+        Command::CacheFastsync { path } => {
+            let tmp_path = format!("{path}.tmp");
+            'next: loop {
+                let remote = tpex_api::Remote::new(args.endpoint.clone(), args.token);
+                let Ok(state_stream) = remote.stream_fastsync().await else {continue;};
+                let mut state_stream = pin!(state_stream);
+                while let Some(next) = state_stream.next().await {
+                    let Ok(next) = next else { continue 'next; };
+                    println!("Id: {}", next.current_id);
+                    // Atomic overwrite of file
+                    tokio::fs::write(&tmp_path, serde_json::to_string(&next).unwrap()).await.expect("Could not write cached data");
+                    std::fs::rename(&tmp_path, &path).expect("Failed to overwrite cached data");
+                }
+            }
+        }
+    }
+
+}
